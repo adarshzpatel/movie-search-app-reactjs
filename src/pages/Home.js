f8916554a467b53bee2a25f83da1c132a0af00d8
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2021-08-26 20:44:31 +0530

    Created Search bar and fetch results from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,30 @@
- import React from 'react'
-import Layout from '../components/Layout'
- 
- function Home() {
-     return (
-         <Layout>
-            Home Page
-         </Layout>
-     )
- }
- 
- export default Home
- 
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+
+function Home() {
+  const [input, setInput] = useState("");
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if(e.keyCode === 13){
+        onSearch();
+    }
+  }
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+  return (
+    <Layout>
+      <input onKeyDown={onKeyDown} value={input} onChange={onInputChange} type="text" />
+        <button onClick={onSearch}>Search</button>
+    </Layout>
+  );
+}
+export default Home;
