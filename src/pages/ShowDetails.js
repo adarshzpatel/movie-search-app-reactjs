8f89ab3bfcd73af418b93fa0f117f28a47420f0b
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2021-08-30 08:06:02 +0530

    replaced useState with useReducer in ShowDetails page

--- src/pages/ShowDetails.js
+++ src/pages/ShowDetails.js
@@ -1,43 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { apiGet } from "../misc/config";
 
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS": {
+      return { error: null, isLoading: false, show: action.show };
+    }
+    case "FETCH_FAILED": {
+      return { ...prevState, error: action.error, isLoading: false };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
 function ShowDetails() {
   const { id } = useParams();
-  const [show, setShow] = useState("null");
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
+  const [{show,isLoading,error},dispatch] = useReducer(reducer, initialState);
+  
   useEffect(() => {
+
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then((results) => {
-        setTimeout(()=>{
-
-          setShow(results);
-          setIsLoading(false);
-        },2000);
+        setTimeout(() => {
+          dispatch({type: 'FETCH_SUCCESS',show:results})
+        }, 2000);
       })
       .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
+        dispatch({type: 'FETCH_FAILED',error: err.message})
+
       });
   }, [id]);
 
-  if(isLoading) {
-    return <div>
-      Fetching Data...
-    </div>;
+  if (isLoading) {
+    return <div>Fetching Data...</div>;
   }
 
-  if(error){
-    return <div>
-      Error : {error}
-    </div>
+  if (error) {
+    return <div>Error : {error}</div>;
   }
-  return (
-  <div>
-  
-  </div>);
+  return <div></div>;
 }
 
 export default ShowDetails;
